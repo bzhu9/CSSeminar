84f6e4578043947e96747385e93c945c2c11a176
Brian Zhu <bzhu@parktudor.org>
2020-05-05 16:37:52 -0400

    make it better

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn channelsTutorial.wsgi
--- channelsTutorial/settings.py
+++ channelsTutorial/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 """
 Django settings for channelsTutorial project.
 
@@ -129,3 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
