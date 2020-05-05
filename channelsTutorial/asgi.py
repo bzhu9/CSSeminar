01a33acb8a152c542966fdd05eb42dfe5d7be52a
Brian Zhu <bzhu@parktudor.org>
2020-05-05 17:09:51 -0400

    make it better

--- channelsTutorial/asgi.py
+++ channelsTutorial/asgi.py
@@ -1,3 +1,8 @@
-from channels.layers import get_channel_layer
+import os
+import django
+from channels.routing import get_default_application
 
-channel_layer = get_channel_layer()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "channelsTutorial.settings")
+django.setup()
+
+application = get_default_application()
