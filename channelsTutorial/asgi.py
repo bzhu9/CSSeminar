3916c90361bc22a6125cb16d0f6cc61a053705a2
Brian Zhu <bzhu@parktudor.org>
2020-05-05 17:24:26 -0400

    make it better

--- channelsTutorial/asgi.py
+++ channelsTutorial/asgi.py
@@ -2,7 +2,7 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "channelsTutorial.settings")
+os.environ['DJANGO_SETTINGS_MODULE'] = "channelsTutorial.settings"
 django.setup()
 
 application = get_default_application()
