21844c20a7861a0c772919555b4ac8398c2727ed
Brian Zhu <bzhu@parktudor.org>
2020-05-06 22:14:40 -0400

    make it better

--- channelsTutorial/settings.py
+++ channelsTutorial/settings.py
@@ -82,6 +82,13 @@ CHANNEL_LAYERS = {
     },
 }
 
+CACHES = {
+    "default": {
+         "BACKEND": "redis_cache.RedisCache",
+         "LOCATION": os.environ.get('REDIS_URL'),
+    }
+}
+
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
