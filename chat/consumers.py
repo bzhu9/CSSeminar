156aa5f6b383ac280175b8262e3301f46123405a
Brian Zhu <bzhu@parktudor.org>
2020-05-17 11:33:47 -0400

    make it better

--- chat/consumers.py
+++ chat/consumers.py
@@ -7,8 +7,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-        self.scope["session"]["seed"] = random.randint(1, 1000)
-        await print(self.channel_layer.group)
+        # self.scope["session"]["seed"] = random.randint(1, 1000)
+        # await print(self.channel_layer.group_channels(room_group_name))
         # Join room group
         await self.channel_layer.group_add(
             self.room_group_name,
