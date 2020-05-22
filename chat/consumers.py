59f39728518c546ec93e9c4366a019943a614dc3
Michelle Chengying Zhu <michelle-chengying.zhu@grenoble-inp.org>
2020-05-22 13:04:18 -0400

    added style for room.html

--- chat/consumers.py
+++ chat/consumers.py
@@ -67,24 +67,28 @@ class ChatConsumer(AsyncWebsocketConsumer):
 	async def receive(self, text_data):
 		text_data_json = json.loads(text_data)
 		message = text_data_json['message']
+		username = text_data_json['username']
 
 		# Send message to room group
 		await self.channel_layer.group_send(
 			self.room_group_name,
 			{
 				'type': 'chat.message',
-				'message': message
+				'message': message,
+				'username' : username
 			}
 		)
 
 	# Receive message from room group
 	async def chat_message(self, event):
 		message = event['message']
+		username = event['username']
 
 		# Send message to WebSocket
 		await self.send(text_data=json.dumps({
 			'msg_type': "message",
-			'message': message
+			'message': message,
+			'username' : username
 		}))
 
 	async def chat_occupants(self, event):
